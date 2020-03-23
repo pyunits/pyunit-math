26943be4e766757b14aaf00bbc92a7168d677295
jtyoui <jtyoui@qq.com>
2020-03-23 11:43:17 +0800

    优化

--- test.py
+++ test.py
@@ -2,3 +2,16 @@
 # -*- coding: utf-8 -*-
 # @Time  : 2020/2/26 17:38
 # @Author: Jtyoui@qq.com
+from pyunit_math import pi
+
+
+def pi_test():
+    s = ''
+    for i in pi(1302):
+        print(i, end='')
+        s += str(i)
+    print('\n', len(s))
+
+
+if __name__ == '__main__':
+    pi_test()
